adcb7e5e6e922e9c21cb316d4f0458889f0805e4
Bryan LEGRAS <blegras@0801209e.local>
2021-05-18 15:27:57 +0200

    JS structure in comms and update index

---
+++ app.js
@@ -0,0 +1,9 @@
+// Etapes : 
+
+// 1- Quand on clique sur le bouton, on va faire :
+// ON va récupérer les divs question (tableaux ?)
+// Pour chaque question : 
+// On regarde pour tous les inputs :
+// Si le input qui est checked contient data-response="true" alors on passe la classe correctAnswer et on incrémente une varaiable bonnes réponses 
+// Si c'est faux on met classe false (animation) avec .toogle
+//A la fin, selon le nb de bonne réponse on modifie la div .resultat en changeant texte et classe
