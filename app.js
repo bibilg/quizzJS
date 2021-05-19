4b113d15c147dec61dc39f5d116b3ec2c05e6bac
Bryan <bryan.legras@gmail.com>
2021-05-19 21:25:20 +0200

    Fetch results

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
-// Etapes : 
+const form = document.querySelector('.form-quizz');
+let tableauResultats = [];
 
-// 1- Quand on clique sur le bouton, on va faire :
-// ON va récupérer les divs question (tableaux ?)
-// Pour chaque question : 
-// On regarde pour tous les inputs :
-// Si le input qui est checked contient data-response="true" alors on passe la classe correctAnswer et on incrémente une varaiable bonnes réponses 
-// Si c'est faux on met classe false (animation) avec .toogle
-//A la fin, selon le nb de bonne réponse on modifie la div .resultat en changeant texte et classe
+form.addEventListener('submit', e => {
+
+    e.preventDefault();
+
+    for(let i=1; i<6 ; i++)
+    {
+        tableauResultats.push(document.querySelector(`.question-block input[name="q${i}"]:checked`).value);
+    }
+
+    console.log(tableauResultats);
+    tableauResultats=[];
+
+});
