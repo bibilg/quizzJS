c5d31acfb580efc802a273dc5b9fd5ffb4e627fd
Bryan <bryan.legras@gmail.com>
2021-05-20 18:10:30 +0200

    Add instructions in comment

--- app.js
+++ app.js
@@ -11,6 +11,26 @@ form.addEventListener('submit', e => {
     }
 
     console.log(tableauResultats);
+
+    //Récupérer les bonnes réponses 
+    //Compter le nombre de bonne réponses, afficher en fonction du nombre de bonne réponses
+
+    //Si bonne réponse, passer style au vert
+    //Si mauvaise réponse, ajouter classe echec et fond en rouge
+
+    //Au bout d'un moment, enlever la classe .animation
+
     tableauResultats=[];
 
 });
+
+    
+//Une fois que c'est de nouveau clique, background en blanc 
+
+
+
+function results(tableauResultats){
+
+
+
+}
